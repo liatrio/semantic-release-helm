8eafe19583a990099493842b4a640a7130b24494
Michael Parker <michael@parker.gg>
2022-01-12 15:54:12 -0600

    refactor: destructure test deps

--- test/cleanup.spec.js
+++ test/cleanup.spec.js
@@ -1,8 +1,8 @@
 jest.dontMock("../src/cleanup");
 
 const { cleanup } = require("../src/cleanup");
-const tempDir = require("../src/util/temp-dir");
-const chartAssets = require("../src/util/chart-assets");
+const { deleteTempDir } = require("../src/util/temp-dir");
+const { deleteChartAssets } = require("../src/util/chart-assets");
 
 describe("cleanup", () => {
     beforeEach(async () => {
@@ -10,10 +10,10 @@ describe("cleanup", () => {
     });
 
     it("should delete the temporary directory that was created for the helm chart assets", () => {
-        expect(tempDir.deleteTempDir).toHaveBeenCalled();
+        expect(deleteTempDir).toHaveBeenCalled();
     });
 
     it("should delete the chart assets that were copied to the main directory for publishing the GitHub release", () => {
-        expect(chartAssets.deleteChartAssets).toHaveBeenCalledWith(context.cwd);
+        expect(deleteChartAssets).toHaveBeenCalledWith(context.cwd);
     });
 });

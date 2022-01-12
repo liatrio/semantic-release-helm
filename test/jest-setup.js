135541af8cf772db9e478b625501dcfb18c9372c
Michael Parker <michael@parker.gg>
2022-01-12 15:36:06 -0600

    test: configure random variables for plugin config and context

--- test/jest-setup.js
+++ test/jest-setup.js
@@ -1,3 +1,12 @@
 const Chance = require("chance");
 
 global.chance = new Chance();
+
+global.pluginConfig = {
+    charts: chance.n(chance.word, chance.d6()),
+    githubPagesBranch: chance.word(),
+};
+
+global.context = {
+    cwd: chance.word()
+};
